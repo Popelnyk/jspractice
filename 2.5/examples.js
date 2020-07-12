2fe13925686cdd893e25b0506831dd71281f41ae
Alexander <alexy-lex2013@yandex.ru>
2020-07-12 15:06:24 +0300

    tasks for chapter 2.5

---
+++ 2.5/examples.js
@@ -0,0 +1,55 @@
+let message = 'hello';
+message = 1234;
+
+// number
+
+let n = 123;
+let fln = 62.3;
+
+console.log(1 / 0);
+console.log(Infinity);
+
+console.log('f' / 1);
+console.log(NaN / 2 + 1);
+
+// bigInt
+// not supported by safari/edge/ie
+const bigNumber = 5823728574285728458345283495n;
+
+// string
+let str1 = 'test string one';
+let str2 = "test string two";
+let phrase = `test string ${str1}`;
+
+console.log(str1, str2, phrase);
+
+// boolean
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+
+console.log(isGreater);
+
+// null
+let age = null;
+console.log(age);
+
+// undefined
+let x;
+console.log(x);
+
+
+// object
+// symbol
+
+
+//-----------------
+console.log(typeof 'lord');
+console.log(typeof alert);
+console.log(typeof Math);
+
+
+
+
+
