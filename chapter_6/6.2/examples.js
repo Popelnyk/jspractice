9c8f9b7de0a7212add0cb10809e53d5903f6c0b3
Alexander <alexy-lex2013@yandex.ru>
2020-08-07 01:42:43 +0300

    examples and tasks for chapter 6.2

---
+++ chapter_6/6.2/examples.js
@@ -0,0 +1,24 @@
+function sumArgs(...args) {
+    let result = 0;
+    for (let item of args) {
+        result += item;
+    }
+
+    return result;
+}
+
+
+console.log(sumArgs(1, 2, 3, 4, 5));
+
+let arr = [1, 3, 5224, 1121, -1111];
+console.log(sumArgs(...arr));
+
+function args1() {
+    let args = Array.from(arguments);
+    console.log(args);
+    args.pop();
+    console.log(...args);
+}
+
+args1(1, 2, 3);
+
